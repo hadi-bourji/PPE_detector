uploading model architecture; removed image normalization, fixing objectness scores

yolox/backbone.py
@@ -6,7 +6,7 @@ Standalone implementation of CSPDarknet backbone
 
 import torch
 import torch.nn as nn
-from network_blocks import BaseConv, CSPLayer, DWConv, Focus, SPPBottleneck
+from .network_blocks import BaseConv, CSPLayer, DWConv, Focus, SPPBottleneck
 
 
 class CSPDarknet(nn.Module):

yolox/head.py
@@ -8,7 +8,7 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from network_blocks import BaseConv, DWConv
+from .network_blocks import BaseConv, DWConv
 
 
 class YOLOXHead(nn.Module):

yolox/model.py
@@ -6,9 +6,9 @@ Complete YOLOX model implementation
 
 import torch
 import torch.nn as nn
-from backbone import CSPDarknet
-from neck import YOLOPAFPN
-from head import YOLOXHead
+from .backbone import CSPDarknet
+from .neck import YOLOPAFPN
+from .head import YOLOXHead
 
 
 class YOLOX(nn.Module):

yolox/neck.py
@@ -6,7 +6,7 @@ Standalone implementation of YOLO-PAFPN neck
 
 import torch
 import torch.nn as nn
-from network_blocks import BaseConv, CSPLayer, DWConv
+from .network_blocks import BaseConv, CSPLayer, DWConv
 
 
 class YOLOPAFPN(nn.Module):

yolox/test_weights.py
@@ -6,7 +6,7 @@ Simple weight loading test for YOLOX-s with variable number of classes
 import os
 import torch
 import requests
-from model import create_yolox_s
+from .model import create_yolox_s
 
 
 def download_weights(save_path="yolox_s.pth"):
@@ -69,6 +69,9 @@ def load_pretrained_weights(model, weights_path, num_classes=None):
     
     # Get model's state dict
     state_dict = map_pretrained_weights(state_dict)
+    # with open("state_dict_mapped.txt", "w") as f:
+    #     for k, v in state_dict.items():
+    #         f.write(f"{k}: {v.shape}\n")
     model_dict = model.state_dict()
     
     # Filter out classification layers if num_classes is different
@@ -101,7 +104,16 @@ if __name__ == "__main__":
 
     # for testing
     weights_path = download_weights()
-    num_classes = 4
+    num_classes = 80
     model = create_yolox_s(num_classes)
-    load_pretrained_weights(model, weights_path, num_classes)
-    print(model(torch.randn(1, 3, 640, 640)).shape)  # Test forward pass
+    model_dict = model.state_dict
+    model = load_pretrained_weights(model, weights_path, num_classes)
+    # state_dict = torch.load(weights_path)["model"]
+    # with open("model_dict.txt", "w") as f:
+    #     for k, v in model_dict().items():
+    #         f.write(f"{k}: {v.shape}\n")
+    # with open("state_dict.txt", "w") as f:
+    #     for k, v in state_dict.items():
+    #         f.write(f"{k}: {v.shape}\n")
+    # load_pretrained_weights(model, weights_path, num_classes)
+    # print(model(torch.randn(1, 3, 640, 640)).shape)  # Test forward pass