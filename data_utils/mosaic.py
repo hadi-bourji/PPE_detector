eef0616ef7d9ca7ae4536dc5cc5ce6c95a5544d5
mattyb709 <matthew.builes8@gmail.com>
2025-07-01 22:06:56 -0500

    updated mosaic for dynamic image sizing

--- data_utils/mosaic.py
+++ data_utils/mosaic.py
@@ -10,6 +10,49 @@ import random
 class Mosaic(nn.Module):
     def __init__(self):
         super(Mosaic, self).__init__()
+        
+    def resize_and_pad_img(self, img, labels, output_width, output_height):
+        
+        new_img = torch.from_numpy(img).float() 
+        new_img = einops.rearrange(new_img, "h w c -> c h w")
+        height, width = new_img.shape[1:]
+
+        scale = min(output_width / width, output_height / height)
+
+        new_width, new_height = int(width * scale), int(height * scale)
+
+        # shrink down the img if it's smaller, otherwise just pad
+        if new_width < width or new_height < height:
+            new_img = F.interpolate(new_img.unsqueeze(0), size=(new_height, new_width), mode='bilinear', align_corners=False)
+
+        # pad with grey (114, 114, 114), not normalized
+        pad_top = (output_height - new_height) // 2
+        pad_bottom = output_height - new_height - pad_top
+        pad_left = (output_width - new_width) // 2
+        pad_right = output_width - new_width - pad_left
+
+        new_img = F.pad(new_img, (pad_left, pad_right, pad_top, pad_bottom), value=114.0)
+
+        # scale labels up to pixel coords, scale by the same refactoring, add padding, then normalize
+        if labels.any():
+            labels[..., 1] = (labels[..., 1] * width * scale + pad_left) / output_width   # xc
+            labels[..., 2] = (labels[..., 2] * height * scale + pad_top) / output_height  # yc
+            labels[..., 3] = (labels[..., 3] * width * scale) / output_width              # w
+            labels[..., 4] = (labels[..., 4] * height * scale) / output_height             # h
+
+        return new_img.squeeze(0), labels
+    
+    def read_img_and_labels(self, img_path):
+
+        new_img = cv2.imread(img_path)
+        lbl_path = img_path.replace('/images/', '/labels/').rsplit('.', 1)[0] + '.txt'
+        if os.stat(lbl_path).st_size == 0:
+            label = np.empty((0, 5), dtype=np.float32)  # empty tensor for no labels
+        else:
+            label = np.loadtxt(lbl_path, dtype=np.float32)
+            if label.ndim == 1:
+                label = label.reshape(1, -1)
+        return new_img, label
 
     def forward(self, img, labels, img_paths, output_size=640):
         """
@@ -30,30 +73,22 @@ class Mosaic(nn.Module):
         """
 
         # Ensure input is a list of images and labels
-                
-        labels = torch.from_numpy(labels)
+
+        if type(labels) == np.ndarray:        
+            labels = torch.from_numpy(labels)
         indices = torch.randperm(len(img_paths))[:3]
         imgs = []
         lbl_list = []
         height, width = img.shape[1:]
-        scale = min(output_size / width, output_size / height)
-        new_width, new_height = int(width * scale), int(height * scale)
-        # images must be of the same size, so we will resize them to the output_size
+
+        # images can be of variable size, so we will make them match the input img size
         for i in indices:
-            new_img = cv2.imread(img_paths[i])
-            new_img = torch.from_numpy(new_img).float() 
-            new_img = einops.rearrange(new_img, "h w c -> c h w")
-            new_img = F.interpolate(new_img.unsqueeze(0), size=(new_height, new_width), mode='bilinear', align_corners=False)
+            new_img, label = self.read_img_and_labels(img_paths[i])
+            new_img, label = self.resize_and_pad_img(new_img, label, output_width=width, output_height=height)
+            
+            assert(new_img.shape[1:] == img.shape[1:])
             imgs.append(new_img.squeeze(0))
 
-
-            lbl_path = img_paths[i].replace('/images/', '/labels/').rsplit('.', 1)[0] + '.txt'
-            if os.stat(lbl_path).st_size == 0:
-                label = np.empty((0, 5), dtype=np.float32)  # empty tensor for no labels
-            else:
-                label = np.loadtxt(lbl_path, dtype=np.float32)
-                if label.ndim == 1:
-                    label = label.reshape(1, -1)
             lbl_list.append(torch.from_numpy(label))
 
         top = torch.cat((img, imgs[0]), dim=2)  # Concatenate horizontally
@@ -65,6 +100,9 @@ class Mosaic(nn.Module):
         w_index = random.randint(0, max(total_width - output_size, 0))
         h_index = random.randint(0, max(total_height - output_size, 0))
         cropped_img = imgs[:, h_index:h_index + output_size, w_index:w_index+output_size]
+        pad_right = max(0, output_size - cropped_img.shape[2])
+        pad_bottom = max(0, output_size - cropped_img.shape[1])
+        cropped_img = F.pad(cropped_img, (0, pad_right, 0, pad_bottom), value=114.0)
 
         fin_labels = torch.empty((0, 5), dtype=torch.float32)
 
@@ -85,7 +123,7 @@ class Mosaic(nn.Module):
 
         # # Top right corner:
         # # TODO how could this be generalized
-        labels = lbl_list[0]
+        labels = lbl_list[0].clone()
         cx, cy, cw, ch = labels[:, 1:2] * width, labels[:, 2:3] * height, labels[:, 3:4] * width, labels[:, 4:5] * height
         labels[:, 1:2] = cx + width - w_index
         labels[:, 2:3] = cy - h_index
@@ -101,7 +139,7 @@ class Mosaic(nn.Module):
 
         fin_labels = torch.cat((fin_labels, labels), dim=0)
 
-        labels = lbl_list[1]
+        labels = lbl_list[1].clone()
 
         # # Bottom left corner:
         cx, cy, cw, ch = labels[:, 1:2] * width, labels[:, 2:3] * height, labels[:, 3:4] * width, labels[:, 4:5] * height
@@ -118,7 +156,7 @@ class Mosaic(nn.Module):
         fin_labels = torch.cat((fin_labels, labels), dim=0)
 
         # # Bottom right corner:
-        labels = lbl_list[2]
+        labels = lbl_list[2].clone()
         cx, cy, cw, ch = labels[:, 1:2] * width, labels[:, 2:3] * height, labels[:, 3:4] * width, labels[:, 4:5] * height
         labels[:, 1:2] = cx + width - w_index
         labels[:, 2:3] = cy + height - h_index
@@ -131,4 +169,5 @@ class Mosaic(nn.Module):
         labels[:, 3] = labels[:, 3] / output_size
         labels[:, 4] = labels[:, 4] / output_size
         fin_labels = torch.cat((fin_labels, labels), dim=0)
+
         return cropped_img, fin_labels
--- data_utils/ppe_dataset.py
+++ data_utils/ppe_dataset.py
@@ -5,9 +5,13 @@ import numpy as np
 import cv2
 import einops
 import os
-
+from .mosaic import Mosaic
+from torchvision.transforms import v2
+import random
+from torchvision import tv_tensors
 class PPE_DATA(Dataset):
-    def __init__(self, data_path: str = "./data", mode="train", max_ground_truth_boxes=30, device = "cuda"):
+    def __init__(self, data_path: str = "./data", mode="train", 
+                 max_ground_truth_boxes=30, p_mosaic = 1/32):
         # read file names from train.txt or validation.txt file
         self.mode = mode
         if mode == "train":
@@ -16,17 +20,42 @@ class PPE_DATA(Dataset):
             self.file_names = np.loadtxt(f"{data_path}/validation.txt", dtype=str)
         else:
             raise Exception("Invalid Mode Entered")
+        
         self.max_gt = max_ground_truth_boxes
-        self.device = device
+        self.p_mosaic = p_mosaic
         self.mosaic = Mosaic()
 
-    def load_and_resize_img(self, img, labels,  output_size = 640):
+
+    def apply_transforms(self, img, labels):
+
+        # apply mosaic augmentation with a probability of p_mosaic
+        if random.random() < self.p_mosaic:
+            img = self.resize_img(img, output_size=640)
+            img, labels = self.mosaic.forward(img, labels, self.file_names, output_size=640)
+        else:
+            img, labels = self.resize_and_pad_img(img, labels, output_size=640)
+        
+        # at this point it should be 640, if not something messed up with mosaic most likely
+        img_size = 640
+        cx, w, y, h = labels[:, 1:2] * img_size, labels[:, 3:4] * img_size, labels[:, 2:3] * img_size, labels[:, 4:5] * img_size
+        x_min = (cx - w / 2)
+        y_min = (y - h / 2)
+        box_labels = torch.cat([x_min, y_min, w, h], dim=1)
+        boxes = tv_tensors.BoundingBoxes(box_labels, format="XYWH", canvas_size=(img_size, img_size))
+
+        return img, labels
+
+    # Used for validaation, just resize and pad
+    def resize_and_pad_img(self, img, labels,  output_size = 640):
 
         height, width = img.shape[1:]
         scale = min(output_size / width, output_size / height)
 
         new_width, new_height = int(width * scale), int(height * scale)
-        img = F.interpolate(img.unsqueeze(0), size=(new_height, new_width), mode='bilinear', align_corners=False)
+
+        # shrink down the img if it's smaller, otherwise just pad
+        if new_width < width or new_height < height:
+            img = F.interpolate(img.unsqueeze(0), size=(new_height, new_width), mode='bilinear', align_corners=False)
 
         # pad with grey (114, 114, 114), not normalized
         pad_top = (output_size - new_height) // 2
@@ -45,6 +74,7 @@ class PPE_DATA(Dataset):
 
         return img.squeeze(0), labels
     
+    # Used for training as input into transforms
     def resize_img(self, img, output_size = 640):
         # resize image to output_size, keeping aspect ratio
         img = torch.from_numpy(img).float() #/ 255.0 # normalize the image
@@ -53,12 +83,14 @@ class PPE_DATA(Dataset):
         scale = min(output_size / width, output_size / height)
 
         new_width, new_height = int(width * scale), int(height * scale)
+        if new_width > width and new_height > height:
+            return img  # no need to resize if img is smaller than output size
+
         img = F.interpolate(img.unsqueeze(0), size=(new_height, new_width), mode='bilinear', align_corners=False)
 
         return img.squeeze(0)
 
-    def __getitem__(self, idx):
-        img_path = self.file_names[idx]
+    def read_img_and_labels(self, img_path):
         img = cv2.imread(img_path)
 
         lbl_path = img_path.replace('/images/', '/labels/').rsplit('.', 1)[0] + '.txt'
@@ -68,10 +100,14 @@ class PPE_DATA(Dataset):
             labels = np.loadtxt(lbl_path, dtype=np.float32)
             if labels.ndim == 1:
                 labels = labels.reshape(1, -1)
+        return img, labels
+
+    def __getitem__(self, idx):
+        img_path = self.file_names[idx]
+        img, labels = self.read_img_and_labels(img_path)
 
         if self.mode == "train":
-            img = self.resize_img(img, output_size=640)
-            img, labels = self.mosaic.forward(img, labels, self.file_names, output_size=640)
+            img, labels = self.apply_transforms(img, labels)
             PPE_DATA.show_img(img, labels, output_path=os.path.join("data_aug", f"mosaic_{idx}.png"),
                               rect_coords_centered=True, normalized=True, show_conf_score=False)
 
@@ -79,15 +115,14 @@ class PPE_DATA(Dataset):
             img, labels = self.load_and_resize_img(img, labels, output_size=640)
 
         if labels.shape[0] > self.max_gt:
-            raise Exception(f"Too many ground truth boxes in {lbl_path}: {labels.shape[0]} > {self.max_gt}")
+            raise Exception(f"Too many ground truth boxes in {img_path}: {labels.shape[0]} > {self.max_gt}")
 
-        # convert labels to tensor, add padding if necessaryjj
+        # convert labels to tensor, add padding if necessary
         if type(labels) == np.ndarray:
             labels = torch.from_numpy(labels)
         padding = torch.ones(self.max_gt - labels.shape[0], 5)
         padding[:, 0] = -1  # set class to -1 for padding
         labels = torch.cat((labels, padding), dim=0)
-        
         
         if self.mode == "val":
             # idx will be used as an img id for metric calculation
@@ -163,4 +198,4 @@ class PPE_DATA(Dataset):
 
 if __name__ == "__main__":
     dataset = PPE_DATA()
-    dataset.__getitem__(3)
+    dataset.__getitem__(5)
